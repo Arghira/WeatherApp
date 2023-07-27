3a57189dc50cca0792bc27bc34c9859d71cab4d4
Arghira Andrei <andrei.arghira@phipartners.com>
2023-07-27 15:01:23 +0300

    Added Helper methods

---
+++ WeatherApp/ViewModel/Helpers/AccuWeatherHelper.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel.Helpers
+{
+    public class AccuWeatherHelper
+    {
+        public const string base_url = "http://dataservice.accuweather.com/";
+        public const string autoComplete_endpoint = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
+        public const string current_condition_endpoint = "currentconditions/v1/{0}?apikey={1}";
+        public const string apiKey = "QhqW6ksVQNTfBsBB6RzOdhcE6RUiFz1W";
+
+        public static async Task<List<City>> GetCities(string query)
+        {
+            string url = $"{base_url}{string.Format(autoComplete_endpoint, apiKey, query)}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode(); // Ensure that the HTTP request was successful.
+
+                string json = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<List<City>>(json);
+            }
+        }
+    }
+
+}
